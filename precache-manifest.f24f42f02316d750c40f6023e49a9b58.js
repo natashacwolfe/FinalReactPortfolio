02ab1fddd9eb965843200f796d44d626b45f0da3
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-06-03 14:27:53 -0700

    Updates

---
+++ precache-manifest.f24f42f02316d750c40f6023e49a9b58.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2d978b7bb49a2debaec6a44d3375f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c2648d09ff832145c9",
+    "url": "/static/css/main.ed8fc663.chunk.css"
+  },
+  {
+    "revision": "c2142b0aec92d9936a44",
+    "url": "/static/js/2.385d0264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.385d0264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c2648d09ff832145c9",
+    "url": "/static/js/main.4e1eaa71.chunk.js"
+  },
+  {
+    "revision": "9b6ceae736d171d535b3",
+    "url": "/static/js/runtime-main.70301a4a.js"
+  },
+  {
+    "revision": "08b9daa5687095705f19c4280e86a19c",
+    "url": "/static/media/Resume.08b9daa5.pdf"
+  },
+  {
+    "revision": "4b92caea78133ffbc9d93de3a868f2cd",
+    "url": "/static/media/eats.4b92caea.png"
+  },
+  {
+    "revision": "0e5932f8389426adc026e09856b31451",
+    "url": "/static/media/home-text.0e5932f8.png"
+  },
+  {
+    "revision": "1093090a9220d819c131b78fabea9f86",
+    "url": "/static/media/natasha.1093090a.jpg"
+  },
+  {
+    "revision": "8afc4d4c5ed3361d437a5ebe506cd118",
+    "url": "/static/media/par.8afc4d4c.png"
+  },
+  {
+    "revision": "ae406b467e9a642399323e1821f111b6",
+    "url": "/static/media/rambler.ae406b46.png"
+  }
+]);
